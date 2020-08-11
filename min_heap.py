4b16af7c341b1ee35b8a162aaabb7bd4e0f61522
tobifanibi <tobifanibi@gmail.com>
2020-08-11 17:59:42 -0500

    add remove_min

--- min_heap.py
+++ min_heap.py
@@ -7,7 +7,6 @@
 # Import pre-written DynamicArray and LinkedList classes
 from a5_include import *
 
-
 class MinHeapException(Exception):
     """
     Custom exception to be used by MinHeap class
@@ -65,13 +64,57 @@ class MinHeap:
         """
         TODO: Write this implementation
         """
-        return None
+        if self.is_empty()==True:
+            return None
+        return self.heap.get_at_index(0)
+
+    def find_min_child(self,index1, index2):
+        try:
+            min1=self.heap.get_at_index(index1)
+        except:
+            min1=None
+
+        try:
+            min2 = self.heap.get_at_index(index2)
+        except:
+            min2 = None
+        if min1==None and min2==None:
+            return None
+        elif min2 == None and min1 != None:
+            return index1
+        elif min1==None and min2!=None:
+            return index2
+        elif min1<min2:
+            return index1
+        elif min2<min1:
+            return index2
 
     def remove_min(self) -> object:
         """
         TODO: Write this implementation
         """
-        return None
+        if self.is_empty():
+            raise MinHeapException
+            return
+        parent_index=0
+        parent=self.get_min()
+        self.heap.swap(parent_index,self.heap.length()-1)
+        self.heap.pop()
+        if self.is_empty():
+            return parent
+        min_child=self.find_min_child(1,2)
+        while min_child!=None:
+            if self.heap.get_at_index(min_child)>self.heap.get_at_index(parent_index):
+                break
+            self.heap.swap(min_child,parent_index)
+            parent_index=min_child
+            if parent_index==None:
+                break
+            min_child=self.find_min_child((parent_index * 2)+1,(parent_index * 2) + 2)
+        return parent
+
+
+
 
     def build_heap(self, da: DynamicArray) -> None:
         """
@@ -99,20 +142,20 @@ if __name__ == '__main__':
         h.add(value)
         print(h)
 
-    #
-    # print("\nPDF - get_min example 1")
-    # print("-----------------------")
-    # h = MinHeap(['fish', 'bird'])
-    # print(h)
-    # print(h.get_min(), h.get_min())
-    #
-    #
-    # print("\nPDF - remove_min example 1")
-    # print("--------------------------")
-    # h = MinHeap([1, 10, 2, 9, 3, 8, 4, 7, 5, 6])
-    # while not h.is_empty():
-    #     print(h, end=' ')
-    #     print(h.remove_min())
+
+    print("\nPDF - get_min example 1")
+    print("-----------------------")
+    h = MinHeap(['fish', 'bird'])
+    print(h)
+    print(h.get_min(), h.get_min())
+
+
+    print("\nPDF - remove_min example 1")
+    print("--------------------------")
+    h = MinHeap([1, 10, 2, 9, 3, 8, 4, 7, 5, 6])
+    while not h.is_empty():
+        print(h, end=' ')
+        print(h.remove_min())
     #
     #
     # print("\nPDF - build_heap example 1")
